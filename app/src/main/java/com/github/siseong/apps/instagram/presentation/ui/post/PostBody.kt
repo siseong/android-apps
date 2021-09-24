52a6e55766adab049d034b944c321c58ab08e111
Seonik Seong <seongsi@gmail.com>
2021-09-25 03:11:10 +0900

    - Add drawables of Instagram iconss - Add App Bar for Instagram Post

---
+++ app/src/main/java/com/github/siseong/apps/instagram/presentation/ui/post/PostBody.kt
@@ -0,0 +1,24 @@
+package com.github.siseong.apps.instagram.presentation.ui.post
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PostBody(
+    count: String,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = count,
+            color = MaterialTheme.colors.onBackground
+        )
+    }
+}
--- app/src/main/java/com/github/siseong/apps/instagram/presentation/ui/post/PostView.kt
+++ app/src/main/java/com/github/siseong/apps/instagram/presentation/ui/post/PostView.kt
@@ -1,27 +1,67 @@
 package com.github.siseong.apps.instagram.presentation.ui.post
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
+import com.github.siseong.apps.R
 
 @Composable
 fun Post() {
     val viewModel: PostViewModel = mavericksViewModel()
 
-    Box(
-        contentAlignment = Alignment.Center,
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = viewModel.collectAsState { it.count.toString() }.value,
-            color = MaterialTheme.colors.onBackground
-        )
+        Row(
+            modifier = Modifier.height(60.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.instagram_logo),
+                contentDescription = "Instagram logo",
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp)
+            )
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.weight(1f)
+                    .fillMaxHeight()
+            ) {
+                MenuItem(R.drawable.ic_instagram_add, "add", {})
+                MenuItem(R.drawable.ic_instagram_activity, "activity", {})
+                MenuItem(R.drawable.ic_instagram_message, "message", {})
+            }
+        }
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            PostBody(viewModel.collectAsState { it.count.toString() }.value)
+        }
     }
+}
+
+@Preview
+@Composable
+fun MenuItem(
+    @DrawableRes resId: Int = R.drawable.ic_instagram_add,
+    contentDescription: String = "",
+    onClick: () -> Unit = {}
+) {
+    Image(
+        painter = painterResource(resId),
+        contentDescription = contentDescription,
+        modifier = Modifier
+            .clickable { onClick() }
+            .size(width = 50.dp, height = 60.dp)
+            .padding(horizontal = 12.dp, vertical = 15.dp)
+    )
 }
